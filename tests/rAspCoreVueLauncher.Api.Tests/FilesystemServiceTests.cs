a0b380da9559f7f1cf061d1c4e92e5fde92cc767
Ronnie <ronnie.sunde@gmail.com>
2026-05-29 15:17:09 +0200

    test: FilesystemService delete routing (trash vs permanent)

---
+++ tests/rAspCoreVueLauncher.Api.Tests/FilesystemServiceTests.cs
@@ -0,0 +1,73 @@
+using FluentAssertions;
+using NSubstitute;
+using rAspCoreVueLauncher.Api.Filesystem;
+using rAspCoreVueLauncher.Shared.Filesystem;
+
+namespace rAspCoreVueLauncher.Api.Tests;
+
+[TestClass]
+public class FilesystemServiceTests
+{
+    private static string NewTempFile()
+    {
+        var path = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+        File.WriteAllText(path, "x");
+        return path;
+    }
+
+    [TestMethod]
+    public void Delete_SoftDelete_RoutesToTrash()
+    {
+        var trash = Substitute.For<IFileTrash>();
+        trash.IsSupported.Returns(true);
+        var service = new FilesystemService(trash);
+        var path = NewTempFile();
+
+        service.Delete(new DeleteRequest(path, Permanent: false));
+
+        trash.Received(1).TrashFile(path);
+        File.Exists(path).Should().BeTrue();
+        File.Delete(path);
+    }
+
+    [TestMethod]
+    public void Delete_Permanent_BypassesTrash()
+    {
+        var trash = Substitute.For<IFileTrash>();
+        trash.IsSupported.Returns(true);
+        var service = new FilesystemService(trash);
+        var path = NewTempFile();
+
+        service.Delete(new DeleteRequest(path, Permanent: true));
+
+        trash.DidNotReceive().TrashFile(Arg.Any<string>());
+        File.Exists(path).Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void Delete_SoftDelete_OnUnsupportedTrash_Throws()
+    {
+        var trash = Substitute.For<IFileTrash>();
+        trash.IsSupported.Returns(false);
+        var service = new FilesystemService(trash);
+        var path = NewTempFile();
+
+        var act = () => service.Delete(new DeleteRequest(path, Permanent: false));
+
+        act.Should().Throw<FilesystemException>()
+            .Which.Error.Should().Be(FilesystemError.TrashUnsupported);
+        File.Exists(path).Should().BeTrue();
+        File.Delete(path);
+    }
+
+    [TestMethod]
+    public void Delete_MissingPath_ThrowsNotFound()
+    {
+        var service = new FilesystemService(Substitute.For<IFileTrash>());
+        var act = () => service.Delete(new DeleteRequest(
+            Path.Combine(Path.GetTempPath(), "does-not-exist-" + Path.GetRandomFileName()), false));
+
+        act.Should().Throw<FilesystemException>()
+            .Which.Error.Should().Be(FilesystemError.NotFound);
+    }
+}
